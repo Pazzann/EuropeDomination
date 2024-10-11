4e11e2b177abc192fd4de2435e170abe8e4dfbbe
Anton <manton2007@gmail.com>
2024-10-11 21:33:46 +0100

    Add steamworks api support

---
+++ Scripts/GlobalStates/SteamworksHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Godot;
+using Steamworks;
+
+namespace EuropeDominationDemo.Scripts.GlobalStates;
+
+public partial class SteamworksHelper : Node
+{
+    public override void _Ready()
+    {
+        GD.Print("Steam is running:" + SteamAPI.IsSteamRunning());
+        try
+        {
+            GD.Print(SteamAPI.Init() ? "Steam API init" : "Steam API init failed");
+        }
+        catch (Exception e)
+        {
+            GD.Print("Steam API Error:" + e.Message);
+        }
+    }
+
+    public override void _ExitTree()
+    {
+        try
+        {
+            SteamAPI.Shutdown();
+        }
+        catch (Exception e)
+        {
+            GD.Print("Steam API Error:" + e.Message);
+        }
+    }
+}
